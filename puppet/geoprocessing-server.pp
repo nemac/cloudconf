d4b5cb64813a4a152fc78e0f7ad0d59a1b169161
Mark Phillips <embeepea@git>
2014-10-23 08:12:06 -0400

    update geoprocessing manifest, switch to yum.nemac.org

--- puppet/geoprocessing-server.pp
+++ puppet/geoprocessing-server.pp
@@ -4,14 +4,34 @@ class { "basic-server" : }
 class { "nappl-server" : }
 class { "apache-server" : }
 
-# package { 'gdal':
-#   ensure => installed
-# }
-# 
-# package { 'gdal-python':
-#   ensure => installed
-# }
-# 
-# package { 'gdal-devel':
-#   ensure => installed
-# }
+package { 'netcdf':
+  ensure => installed
+}
+
+package { 'netcdf-devel':
+  ensure => installed
+}
+
+package { 'netcdf4-python'
+  ensure => installed
+}
+
+package { 'proj':
+  ensure => installed
+}
+
+package { 'proj-devel':
+  ensure => installed
+}
+
+package { 'nemac-gdal':
+  ensure => installed
+}
+
+package { 'nemac-gdal-devel':
+  ensure => installed
+}
+
+package { 'nemac-gdal-python':
+  ensure => installed
+}
--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -20,8 +20,22 @@ if test ! -f /etc/yum.repos.d/epel.repo ; then
    yum -y install epel-release
 fi
 
+# The following installs the NEMAC yum repository.  This obviously depends
+# on the set yum.nemac.org being up and running.  If you are bootstrapping
+# the server for that site itself, though, presumably it won't be up and
+# running when this script runs.  To work around that:
+#   1. put a copy of the git repo github.com:nemac/yum.nemac.org on ANY
+#      public web server
+#   2. change the address here to that server
+#   3. on that server, edit the file nemac.repo to change yum.nemac.org
+#      to the address of that server
+#   4. configure the server (this step involves running this script)
+#   5. install the yum.nemac.org site on the server
+#   6. edit the file /etc/yum.repos.d/nemac.repo on the server to change
+#      the URL back to yum.nemac.org
+#   7. remove the temporary copy of the site that you created in step 1
 if test ! -f /etc/yum.repos.d/nemac.repo ; then
-  cp /etc/puppet/files/assets/nemac/nemac.repo /etc/yum.repos.d
+  curl http://yum.nemac.org/nemac.repo > /etc/yum.repos.d/nemac.repo
 fi
 
 exit 0
